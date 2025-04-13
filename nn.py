8e5b3ec59df5097038b3e890d5eadea8e0bb8c6c
Sid <sidsurakanti1@gmail.com>
2025-04-12 21:43:30 -0400

    impr: updated nn performance + added notes + requirements.txt + updated .py file

--- nn.py
+++ nn.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+from random import randint
 
 # %%
 train = pd.read_csv("./dataset/train.csv")
@@ -46,35 +47,37 @@ def one_hot(y):
 
 # %%
 def forward_pass(W1, b1, W2, b2, X):
-    Z1 = W1 @ X + b1
-    A1 = ReLU(Z1)
-    Z2 = W2 @ A1 + b2
-    A2 = softmax(Z2)
+    Z1 = W1 @ X + b1 # (10, m)
+    A1 = ReLU(Z1) # hidden layer 1 (10, m)
+    Z2 = W2 @ A1 + b2 # logits, (10, m)
+    A2 = softmax(Z2) # output layer normalized based on probability (10, m)
 
     return A2, Z2, A1, Z1
 
 # %%
-def backward_pass(A2, Z2, A1, Z1, W2, W1, y):
-    dZ2 = 2 * (A2 - one_hot(y))
-    dW2 = dZ2 @ A1.T / m
-    db2 = np.sum(dZ2, axis=1, keepdims=True) / m
-    
-    dA1 = W2.T @ dZ2
-    dZ1 = dA1 * dReLU(Z1)
-    dW1 = dZ1 @ X.T / m
-    db1 = np.sum(dZ1, axis=1, keepdims=True) / m
-
-    return dW1, db1, dW2, db2
-
 def cost(A2, y):
     return (A2 - one_hot(y))**2 * 1/m
 
+def backward_pass(A2, Z2, A1, Z1, W2, W1, y):
+    # chain rule dC/dW2, dC/db2
+    dZ2 = 2 * (A2 - one_hot(y)) # dC wrt dZ2
+    dW2 = dZ2 @ A1.T / m # (10, m) * (m, 10).T => (10, 10), dZ2 wrt dW2
+    db2 = np.sum(dZ2, axis=1, keepdims=True) / m # (10, 1)
+
+    # chain rule dC/dW1, dC/db1
+    dA1 = W2.T @ dZ2 # (10, 10) * (10, m) => (10, m)
+    dZ1 = dA1 * dReLU(Z1) # => (10, m)
+    dW1 = dZ1 @ X.T / m  # (10, m) * (m, 784) => (10, 784)
+    db1 = np.sum(dZ1, axis=1, keepdims=True) / m # (10, 1)
+
+    return dW1, db1, dW2, db2
+
 def update_params(dW1, db1, dW2, db2, W2, b2, W1, b1):
-    lr = 0.1
-    W2 -= dW2 * lr
-    b2 -= db2 * lr
-    W1 -= dW1 * lr
-    b1 -= db1 * lr
+    alpha = 0.1 # learning rate
+    W2 -= dW2 * alpha
+    b2 -= db2 * alpha
+    W1 -= dW1 * alpha
+    b1 -= db1 * alpha
 
     return W1, b1, W2, b1
 
@@ -90,12 +93,12 @@ def gradient_descent(X, Y, iterations):
         W1, b1, W2, b2 = update_params(dW1, db1, dW2, db2, W2, b2, W1, b1)
 
         if i % 50 == 0:
-            print("Iteration :", i)
-            print("Accuracy :", round(get_accuracy(A2, Y), 2))
+            print("Iteration:", i)
+            print("Accuracy:", round(get_accuracy(A2, Y), 2))
     return W1, b1, W2, b2
 
 # %%
-trainedWeights = gradient_descent(X, y, 500)
+trainedWeights = gradient_descent(X, y, 10000)
 
 # %%
 def predict(i, W1, b1, W2, b2):
@@ -104,9 +107,9 @@ def predict(i, W1, b1, W2, b2):
     label = y_test[i]
     plt.imshow(X_test[i].reshape((28, 28)), cmap="gray")
     plt.axis(False)
-    print("Prediction: ", int(prediction))
+    print("Prediction:", int(prediction))
     print("Label:", int(label))
 
-predict(24, *trainedWeights)
+predict(randint(1, 10000), *trainedWeights)
 
 
